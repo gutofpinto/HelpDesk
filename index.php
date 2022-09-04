45688426f2b80f74d62a542372e86d09cd77d494
Augusto_F_P <106616580+gutofpinto@users.noreply.github.com>
2022-09-04 15:54:46 -0300

    0.1
    formatação

--- index.php
+++ index.php
@@ -13,19 +13,17 @@
       }
     </style>
   </head>
-
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
     </nav>
 
     <div class="container">    
       <div class="row">
-
         <div class="card-login">
           <div class="card">
             <div class="card-header">
@@ -34,7 +32,7 @@
             <div class="card-body">
               <form>
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input type="text" class="form-control" placeholder="Usuário">
                 </div>
                 <div class="form-group">
                   <input type="password" class="form-control" placeholder="Senha">
--- view/abrir_chamado.php
+++ view/abrir_chamado.php
@@ -2,9 +2,7 @@
   <head>
     <meta charset="utf-8" />
     <title>App Help Desk</title>
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <style>
       .card-abrir-chamado {
         padding: 30px 0 0 0;
@@ -13,19 +11,15 @@
       }
     </style>
   </head>
-
   <body>
-
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="../img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="logo.png">
         App Help Desk
       </a>
     </nav>
-
     <div class="container">    
       <div class="row">
-
         <div class="card-abrir-chamado">
           <div class="card">
             <div class="card-header">
@@ -34,13 +28,11 @@
             <div class="card-body">
               <div class="row">
                 <div class="col">
-                  
                   <form>
                     <div class="form-group">
                       <label>Título</label>
                       <input type="text" class="form-control" placeholder="Título">
                     </div>
-                    
                     <div class="form-group">
                       <label>Categoria</label>
                       <select class="form-control">
@@ -51,23 +43,19 @@
                         <option>Rede</option>
                       </select>
                     </div>
-                    
                     <div class="form-group">
                       <label>Descrição</label>
                       <textarea class="form-control" rows="3"></textarea>
                     </div>
-
                     <div class="row mt-5">
                       <div class="col-6">
                         <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
                       </div>
-
                       <div class="col-6">
                         <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
                       </div>
                     </div>
                   </form>
-
                 </div>
               </div>
             </div>
